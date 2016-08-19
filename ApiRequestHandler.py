3eef90f35db7d8e60b9fe8f9379522ac68e2f404
ckimmons <ckimmons@usc.edu>
2016-08-19 17:09:03 -0500

    addedcomments to ApiRequestHandler.py

--- ApiRequestHandler.py
+++ ApiRequestHandler.py
@@ -2,17 +2,21 @@ import requests
 import json
 TOKEN = '0bb28905c84012d47d113eb090120e12'
 
+# Recieves a string from the CODE2040 assessment server.
 def RecieveProblem(url):
     my_dictionary = {'token': TOKEN}
     request = requests.post(url, data=my_dictionary);
     return request.text
 
+# Creates a dictionary from the key and value, and sends the
+# POST request to the url meant to validate stages.
 def ValidateProblem(url, key, value):
     my_dictionary ={'token' : TOKEN, key : value}
     print(my_dictionary)
     output = requests.post(url, json=my_dictionary);
     print(output.text)
 
+# Turns the string recieved from RecievedProblem into a usable dictionary.
 def JsonLoader(text):
     dictionary = json.loads(text)
     return dictionary
